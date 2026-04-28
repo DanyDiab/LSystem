7410808bdcc14e1040aa5c71620d863400e502a5
DanyLaptop <DanyDiab04@gmail.com>
2026-04-28 13:07:06 -0300

    turtle system started

--- scripts/renderer.cpp
+++ scripts/renderer.cpp
@@ -15,14 +15,107 @@ std::string filePath = "./instructions.json";
 const int length = 2;
 const float DEGTORAD = 0.01745329f;
 
-void drawLine(float x, float y, int theta){
-    float endX = cos((90 - theta) * DEGTORAD) * length;
-    float endY = sin((90 - theta) * DEGTORAD) * length;
+struct Turtle {
+    int x;
+    int y;
+    int theta;
+};
+
+Turtle turtle;
+Turtle nextTurtle;
+
+void init(){
+    turtle = {0, 0, 0};
+    nextTurtle = {0, 0, 0};
+}
+
+void moveTurtleForward(Turtle turtle){
+    int theta = turtle.theta;
+    float offsetX = cos((90 - theta) * DEGTORAD) * length;
+    float offsetY = sin((90 - theta) * DEGTORAD) * length;
+
+    float endX = offsetX + turtle.x;
+    float endY = offsetY + turtle.y;
+
+    turtle.x = endX;
+    turtle.y = endY;
+}
+
+void executeInstruction(Token token){
+    switch (token) {
+        case Token::F: {
+            moveTurtleForward(nextTurtle);
+            drawLineBetweenTurtles(turtle,nextTurtle);
+            break;
+        }
+        case Token::G: {
+            moveTurtleForward(nextTurtle);
+            drawLineBetweenTurtles(turtle,nextTurtle);
+            break;
+        }
+        case Token::f: {
+            moveTurtleForward(nextTurtle);
+            break;
+        }
+        case Token::Z: {
+            break;
+        }
+        case Token::TurnLeft: {
+            break;
+        }
+        case Token::TurnRight: {
+            break;
+        }
+        case Token::TurnAround: {
+            break;
+        }
+        case Token::PitchDown: {
+            break;
+        }
+        case Token::PitchUp: {
+            break;
+        }
+        case Token::RollLeft: {
+            break;
+        }
+        case Token::RollRight: {
+            break;
+        }
+        case Token::PushState: {
+            break;
+        }
+        case Token::PopState: {
+            break;
+        }
+        case Token::DecreaseWidth: {
+            break;
+        }
+        case Token::IncreaseWidth: {
+            break;
+        }
+        case Token::NextColor: {
+            break;
+        }
+        case Token::DrawPolygon: {
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+}
+
+void drawLineBetweenTurtles(Turtle turtle1, Turtle turtle2){
+    int startX = turtle1.x;
+    int startY = turtle1.y;
+
+    int endX = turtle2.x;
+    int endY = turtle2.y;
     glLineWidth(10);
     glColor3f(1.0,1.0,1.0);
     glBegin(GL_LINES);
-        glVertex2f(x, y);
-        glVertex2f(x + endX, y + endY);
+        glVertex2f(startX, startY);
+        glVertex2f(endX, endY);
     glEnd();
 
     glFlush(); 
@@ -31,17 +124,10 @@ void drawLine(float x, float y, int theta){
 void update(){
     glutSwapBuffers();
     glutPostRedisplay();
-    drawLine(0,.5,0);
 }
-
-
-
-
 
 std::vector<Token> readInJSON(){
     std::ifstream file(filePath);
-    std::string line;
-
 
     std::vector<Token> instructions;
     if(!file.is_open()){
@@ -54,11 +140,11 @@ std::vector<Token> readInJSON(){
 
     instructions = parsedData["instructions"].get<std::vector<Token>>();
     return instructions;
-    
 }
 
 int main(int argc, char** argv){
     readInJSON();
+    init();
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(600, 600);
@@ -66,6 +152,8 @@ int main(int argc, char** argv){
     glutDisplayFunc(update);
 
     glutMainLoop();
+
+    
 
     return 0;
 }
