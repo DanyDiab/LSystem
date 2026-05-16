d9de57d398e6d7fca5e968b17ae3701b3f169ca8
DanyDiab <danydiab04@gmail.com>
2026-05-16 14:43:15 -0300

    implemented paraInstrucitons, working on moving generator to paraSystem

--- scripts/generator.cpp
+++ scripts/generator.cpp
@@ -6,56 +6,159 @@
 #include <vector>
 #include <sstream>
 #include <map>
+#include <cstdlib>
+#include <stdexcept>
+
+
+#include "./headers/Util.hpp"
 #include "./headers/tokens.hpp"
 #include "./headers/rule.hpp"
-#include <cstdlib>
 
 using namespace std;
 using json = nlohmann::json;
 
 int MAXDEPTH = 5;
 
-std::string recurExpand(std::string curr, std::vector<Rule> rules, int numRules, int depth){
-    if(depth == MAXDEPTH) return curr;
-    std::string nextExpansion;
+// std::string recurExpand(std::string curr, std::vector<Rule> rules, int numRules, int depth){
+//     if(depth == MAXDEPTH) return curr;
+//     std::string nextExpansion;
     
-    for(const auto& character : curr){
-        if(std::isspace(character)) continue;
+//     for(const auto& character : curr){
+//         if(std::isspace(character)) continue;
 
-        bool foundExpansion = false;
-        for(const auto& rule : rules){
-            if(rule.LHS != character) continue;
+//         bool foundExpansion = false;
+//         for(const auto& rule : rules){
+//             if(rule.LHS != character) continue;
 
-            foundExpansion = true;
-            float rand = static_cast<float>(std::rand() / static_cast<float>(RAND_MAX));
+//             foundExpansion = true;
+//             float rand = static_cast<float>(std::rand() / static_cast<float>(RAND_MAX));
 
-            std::vector<float> probs = rule.probs;
-            std::vector<string> out = rule.RHS;
+//             std::vector<float> probs = rule.probs;
+//             std::vector<string> out = rule.RHS;
             
-            int size = probs.size();
-            float runningProb = 0.0f;
-            for(int i = 0; i < size; i++){
-                runningProb += probs.at(i);
+//             int size = probs.size();
+//             float runningProb = 0.0f;
+//             for(int i = 0; i < size; i++){
+//                 runningProb += probs.at(i);
 
-                if(rand >= runningProb) continue;
+//                 if(rand >= runningProb) continue;
                 
-                nextExpansion += out.at(i);
-                break;
+//                 nextExpansion += out.at(i);
+//                 break;
                 
-            }
-            break;
+//             }
+//             break;
+//         }
+//         if(!foundExpansion){
+//             nextExpansion += character;
+//         }
+//     }
+
+//     return recurExpand(nextExpansion,rules,numRules, depth+1);
+// }
+
+// std::string generateExpansion(std::tuple<string, std::vector<Rule>, float> data){
+//     std::string axiom = std::get<0>(data);
+//     std::vector<Rule> rules = std::get<1>(data);
+//     int numRules = rules.size();
+//     return recurExpand(axiom,rules,numRules,0);
+// }
+
+ParaInstruction* encodeInstruction(std::string instructionToEncode){
+    ParaInstruction* paraIns = new ParaInstruction();
+    
+    std::vector<char> delim = {'('};
+    std::vector<char> paramDelim = {',', ')', ' '};
+
+    std::vector<std::string> split = Util::splitString(instructionToEncode, delim);
+
+    char token = split[0][0];
+
+    std::string params = split[1];
+    std::vector<std::string> paramSplit = Util::splitString(params,paramDelim);
+
+    std::vector<std::variant<float, std::string>> paramsVariant;
+
+
+    for(const auto& param : paramSplit){
+        if(Util::isNumeric(param)){
+            float paramF = std::stof(param);
+            cout << "this is a float: " << paramF << "\n";
+            paramsVariant.push_back(paramF);
         }
-        if(!foundExpansion){
-            nextExpansion += character;
+        else{
+            cout << "this is a string: " << param << "\n";
+            paramsVariant.push_back(param);
         }
     }
 
-    return recurExpand(nextExpansion,rules,numRules, depth+1);
+    paraIns->token = token;
+    paraIns->params = paramsVariant;
+    return paraIns;
+
+}
+
+// <axiom, rules>
+std::tuple<ParaInstruction*, std::vector<Rule>> parseJSON(){
+    std::ifstream file("./systems/paraSystem.json");
+
+    std::vector<Rule> rules;
+    std::map<std::string, float> constants;
+    std::vector<ParaInstruction*> RHSVec;
+    ParaInstruction* axiomIns = new ParaInstruction();
+    if (!file.is_open()){
+        cout << "file was not found or didnt open";
+        return std::make_tuple(axiomIns, rules);
+    } 
+
+    json parsedData;
+    file >> parsedData;
+
+    std::string axiomStr = parsedData["axiom"].get<std::string>();
+
+    axiomIns = encodeInstruction(axiomStr);
+    constants = parsedData["constants"].get<std::map<std::string, float>>();
+    
+    json rulesJson = parsedData["rules"];
+    for(json::iterator it = rulesJson.begin(); it != rulesJson.end(); it++){
+        Rule rule;
+        std::string keyString = it.key();
+
+        ParaInstruction* key = encodeInstruction(keyString);
+
+        rule.LHS = key;
+
+        json outcomes = it.value();
+
+        std::vector<float> probs;
+        std::vector<std::string> outs;
+        for(const auto& outcome : outcomes){
+            float prob = outcome["prob"].get<float>();
+            
+            probs.push_back(prob);
+            
+            
+            std::string out = outcome["out"].get<std::string>();
+            
+            std::vector<std::string> outTokens = Util::tokenize(out);
+
+            for(const auto& tok : outTokens){
+                ParaInstruction* paraIns = encodeInstruction(tok);
+                RHSVec.push_back(paraIns);
+            }
+            
+        }
+        rule.RHS = RHSVec;
+        rule.probs = probs;
+        rules.push_back(rule);
+    }
+
+    file.close();
+
+    return std::make_tuple(axiomIns, rules);
 }
 
 int writeInstructionsToJSON(std::string expanded, float theta){
-
-
     std::vector<Token> tokens;
 
     for(const auto& c : expanded){
@@ -77,72 +180,14 @@ int writeInstructionsToJSON(std::string expanded, float theta){
     return 0;
 }
 
-std::string generateExpansion(std::tuple<string, std::vector<Rule>, float> data){
-    std::string axiom = std::get<0>(data);
-    std::vector<Rule> rules = std::get<1>(data);
-    int numRules = rules.size();
-    return recurExpand(axiom,rules,numRules,0);
-}
-
-// <axiom, rules>
-std::tuple<string, std::vector<Rule>, float> parseJSON(){
-    std::ifstream file("./systems/system.json");
-
-    std::string axiom;
-    std::vector<Rule> rules;
-    float theta = 0.0f;
-
-    if (!file.is_open()){
-        cout << "file was not found or didnt open";
-        return std::make_tuple(axiom, rules, theta);
-    } 
-
-    json parsedData;
-    file >> parsedData;
-
-    axiom = parsedData["axiom"].get<std::string>();
-    theta = parsedData["theta"].get<float>();
-
-    json rulesJson = parsedData["rules"];
-
-    for(json::iterator it = rulesJson.begin(); it != rulesJson.end(); it++){
-        Rule rule;
-        std::string keyString = it.key();
-        char keyChar = keyString[0];
-
-        rule.LHS = keyChar;
-
-        json outcomes = it.value();
-
-        std::vector<float> probs;
-        std::vector<std::string> outs;
-        for(const auto& outcome : outcomes){
-
-            float prob = outcome["prob"].get<float>();
-            std::string out = outcome["out"].get<std::string>();
-
-            probs.push_back(prob);
-            outs.push_back(out);
-        }
-
-        rule.probs = probs;
-        rule.RHS = outs;
-        rules.push_back(rule);
-    }
-
-    file.close();
-
-    return std::make_tuple(axiom, rules, theta);
-}
-
 int main(int argc, char** argv){
     unsigned int currentTime = static_cast<unsigned int>(time(nullptr));
     srand(currentTime);
-    std::tuple<string, std::vector<Rule>, float> data = parseJSON();
-    std::string expanded = generateExpansion(data);
+    std::tuple<ParaInstruction*, std::vector<Rule>> data = parseJSON();
+    // std::string expanded = generateExpansion(data);
 
-    float theta = std::get<2>(data);
-    writeInstructionsToJSON(expanded, theta);
+    // float theta = std::get<2>(data);
+    // writeInstructionsToJSON(expanded, theta);
     
     return 0;
 }
--- scripts/headers/rule.hpp
+++ scripts/headers/rule.hpp
@@ -1,15 +1,22 @@
-#ifndef RULEHPP
-#define RULEHPP
+#pragma once
 
 #include <vector>
 #include <string>
+#include <variant>
 
 
-struct Rule{
-    char LHS;
-    std::vector<float> probs;
-    std::vector<std::string> RHS;    
+
+
+// individual instrucitons in the system get parsed down to this
+// float if it is constant
+// char if it is a parameter of the rule
+struct ParaInstruction{
+    char token;
+    std::vector<std::variant<float, std::string>> params;
 };
 
-
-#endif
+struct Rule{
+    ParaInstruction* LHS;
+    std::vector<float> probs;
+    std::vector<ParaInstruction*> RHS;    
+};
